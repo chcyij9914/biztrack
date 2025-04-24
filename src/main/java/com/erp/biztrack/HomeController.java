c119ffee1cc98a2f4cde0a56731d66a457e1080b
chcyij9914 <chcyij9914@naver.com>
2025-04-24 18:30:14 +0900

    Revert "구매문서목록 표시"

--- src/main/java/com/erp/biztrack/HomeController.java
+++ src/main/java/com/erp/biztrack/HomeController.java
@@ -1,10 +1,6 @@
 package com.erp.biztrack;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,12 +10,4 @@ public class HomeController {
 	public String forwardMainView() {
 		return "common/main";
 	}
-	
-	 // 구매문서 목록 조회
-    @GetMapping("/purchase/purchase-document.do")
-    public String showPurchaseList(Model model) {
-        List<PurchaseDTO> list = purchaseService.getPurchaseList(); // 서비스에 요청
-        model.addAttribute("purchaseList", list); // 모델에 담아서 JSP에 전달
-        return "purchase/purchase-document"; // 뷰 이름
-    }
 }
