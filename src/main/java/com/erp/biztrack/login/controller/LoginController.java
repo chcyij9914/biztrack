94e8f43cb74c6750a2912d52777cd3c55a9fc5ff
Geonho0719 <kknh97@naver.com>
2025-05-16 12:40:54 +0900

    다시 보냄 김건호의 의지

--- src/main/java/com/erp/biztrack/login/controller/LoginController.java
+++ src/main/java/com/erp/biztrack/login/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,13 +24,13 @@ public class LoginController {
 	private LoginService loginService;
 
 	// 로그인 페이지
-	@GetMapping("login.do")
+	@RequestMapping("login.do")
 	public String loginPage() {
 		return "login/login";
 	}
 
 	// 로그인 검증 (POST로 처리)
-	@PostMapping("loginCheck.do")
+	@RequestMapping(value="loginCheck.do", method=RequestMethod.POST)
 	public String loginCheck(@RequestParam("empId") String empId, @RequestParam("empPwd") String empPwd,
 			HttpSession session, Model model) throws Exception {
 		LoginDto dto = new LoginDto();
@@ -62,7 +64,7 @@ public class LoginController {
 	}
 
 	// 로그아웃
-	@GetMapping("/logout.do")
+	@RequestMapping("/logout.do")
 	public String logout(HttpSession session) {
 		session.invalidate(); // 세션 무효화 (로그아웃)
 		return "redirect:/login.do"; // 로그인 페이지로 이동
@@ -70,7 +72,7 @@ public class LoginController {
 
 	// 비밀번호 찾기 페이지------------------------------------------------------------
 	// 로그인 페이지
-		@GetMapping("findPassword.do")
+	@RequestMapping("findPassword.do")
 		public String findPassword() {
 			return "login/findPassword";
 		}
@@ -78,7 +80,7 @@ public class LoginController {
 	
 	
 	//비밀번호 변경 페이지
-	@GetMapping("/changePassword.do")
+	@RequestMapping("/changePassword.do")
     public String changePasswordPage(HttpSession session, Model model) {
         LoginDto loginUser = (LoginDto) session.getAttribute("loginInfo");
         if (loginUser == null) return "redirect:/login.do";
@@ -86,7 +88,7 @@ public class LoginController {
         return "login/updatePassword";
     }
 
-    @PostMapping("/updatePassword.do")
+	@RequestMapping(value="updatePassword.do", method=RequestMethod.POST)
     public String updatePassword(@RequestParam("empId") String empId,
                                  @RequestParam("newPwd") String newPwd,
                                  @RequestParam("confirmPwd") String confirmPwd,
@@ -122,7 +124,7 @@ public class LoginController {
         }
     }
     
-    @PostMapping("/sendAuthCode.do")
+    @RequestMapping(value="sendAuthCode.do", method=RequestMethod.POST)
     @ResponseBody
     public String sendAuthCode(@RequestParam String empId,
                                @RequestParam String email,
@@ -141,7 +143,7 @@ public class LoginController {
     }
     
     // 비밀번호 찾기 페이지 ( --------------------------------------------------
-    @PostMapping("/verifyCode.do")
+    @RequestMapping(value="verifyCode.do", method=RequestMethod.POST)
     public String verifyAuthCode(@RequestParam String authCode,
                                  HttpSession session,
                                  Model model) {
